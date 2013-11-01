6777b55a5e49da1f840c50a763158881b694ecfa
Fabio Anderegg <andef4@bfh.ch>
2013-11-01 15:42:48 +0100

    initial working socket connection with node

--- src/ts/auth.ts
+++ src/ts/auth.ts
@@ -1,20 +1,53 @@
-import packet_factory = require("packet_factory");
+/// <reference path="../lib/node.d.ts" />
+import net = require("net");
 
+import constants = require("./constants");
+import packet_factory = require("./packet_factory");
 
+function print(data: Uint8Array): string {
+    var str: string = "";
+    for(var i = 0; i < data.length; i++) {
+        var c = data[i].toString(16);
+        if (c.length == 1) {
+            str += "0";
+        }
+        str += c;
+        str += " ";
+    }
+    return str;
+}
 
-export function auth(username: string, password: string): Uint8Array {
-    /*var data = WA.Network.PacketFactory.helloPacket();
-    var str = print(data);
+function arrayToBuffer(array: Uint8Array) {
+    var buffer = new Buffer(array.length);
+    for(var i = 0; i < array.length; i++) {
+        buffer[i] = array[i];
+    }
+    return buffer;
+}
+
+export function auth(username: string, password: string): void {
     
-    var packet = WA.Network.PacketFactory.streamStartPacket();
-    data = packet.serialize();
-    str += print(data);
+    var socket = net.connect(constants.PORT, constants.HOST);
     
-    packet = WA.Network.PacketFactory.featuresPacket();
-    data = packet.serialize();
-    str += print(data);
-    */
-    var packet = packet_factory.authPacket(username);
-    var data = packet.serialize();
-    return data;
+    socket.on("connect", function() {
+        var data = packet_factory.helloPacket();
+        socket.write(arrayToBuffer(data));
+        
+        var packet = packet_factory.streamStartPacket();
+        socket.write(arrayToBuffer(packet.serialize()));
+        
+        packet = packet_factory.featuresPacket();
+        socket.write(arrayToBuffer(packet.serialize()));
+        
+        packet = packet_factory.authPacket(username);
+        socket.write(arrayToBuffer(packet.serialize()));
+        
+        // ret = 00 00 05 f8 03 01 41 ab 00 00 0d f8 02 bb f8 02 f8 01 9c f8 03 e4 cb 0c 00 00 1b f8 04 1b e8 cf fc 14 7a bd d7 6d 6c 0f ff bd 7f 26 9e 94 f9 7d f6 0b 89 07 72 84
+    });
+    
+    socket.on("data", function(data) {
+        console.log(print(data));
+        console.log("");
+        console.log("");
+    });
 }
---
+++ src/ts/node_main.ts
@@ -0,0 +1,4 @@
+import auth = require("./auth");
+import credentials = require("./credentials");
+
+auth.auth(credentials.username, credentials.password);
