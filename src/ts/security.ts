de6ad76eb51c1a3905e69484907e2abc6352fe02
andef4 <andef4@bfh.ch>
2014-01-14 02:40:21 +0100

    use own implementation of RC4

--- src/ts/security.ts
+++ src/ts/security.ts
@@ -1,3 +1,5 @@
+import helpers = require("./helpers");
+
 export function keyFromPasswordNonce(crypto: ICrypto, password: string, nonce: string): string {
     return crypto.PBKDF2(password, nonce, 5, 16);
 }
@@ -26,12 +28,12 @@ export function authBlob(username: string, nonce: string): Uint8Array {
 
 export class KeyStream {
     private crypto: ICrypto;
-    private rc4: IRC4;
+    private rc4: RC4Drop;
     private key: string;
     constructor(crypto: ICrypto, key: string) {
         this.crypto = crypto;
         this.key = key;
-        this.rc4 = this.crypto.RC4(key, 256);
+        this.rc4 = new RC4Drop(key, 256);
     }
     
     decrypt(data: Uint8Array): Uint8Array {
@@ -39,14 +41,14 @@ export class KeyStream {
     }
     
     encrypt(data: Uint8Array, dataOffset: number, macOffset: number): Uint8Array {
-        var encryptedData = this.rc4.encrypt(data);
+        var encryptedData = this.rc4.cipher(data);
         var hmac = this.crypto.HmacSHA1(this.key, encryptedData);
         
         var ret = new Uint8Array(data.length + 4);
         for (var i = macOffset; i < macOffset + 4; i++) {
             ret[i] = hmac[i - macOffset];
         }
-        for (var i = dataOffset; i < encryptedData.length; i++) {
+        for (var i = dataOffset; i < encryptedData.length + dataOffset; i++) {
             ret[i] = encryptedData[i - dataOffset];
         }
         return ret;
